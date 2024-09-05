38712105cd61010f90087c37e7f415e1c3f0d8b9
John Argandoña <116611187+Neroj66@users.noreply.github.com>
2024-09-05 10:05:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
  
-app.use(cors());
+app.use(cors({
+    origin: "*", // Permite todas las orígenes
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 app.use('/', routes);
 
